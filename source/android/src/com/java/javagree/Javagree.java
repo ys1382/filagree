ba9522e4d295512c44d99a063a0e4bd8cf0a2318
yusuf <ys1382@gmail.com>
2013-09-14 03:12:32 -0700

    android callback works more

--- source/android/src/com/java/javagree/Javagree.java
+++ source/android/src/com/java/javagree/Javagree.java
@@ -1,14 +1,7 @@
 package com.java.javagree;
 
-//import java.lang.reflect.Method;
-//import java.util.ArrayList;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Iterator;
-
-import android.content.Context;
-import android.util.Log;
 
 /**
  *
@@ -33,54 +26,6 @@ public class Javagree {
     */
    public native int eval(Object callback, String name, String program, Object sys);
 
-    /**
-    *
-    * Sys
-    *
-    * filagree -> OS bridge. Should be extended to implement Android-specific HAL functions.
-    * 
-    */
-   static class Sys {
-
-       private Context context;
-
-       public Sys(Context context) { this.context = context; }
-
-       public String read(String name) {
-    	   
-    	   Log.d("Javagree", "read " + name);
-
-           InputStream input;
-           
-           try {
-
-        	   input = this.context.getAssets().open(name);
-
-               int size = input.available();
-               byte[] buffer = new byte[size];
-               input.read(buffer);
-               input.close();
-
-              return new String(buffer);
-
-           } catch (IOException e) {
-               e.printStackTrace();
-               return "";
-           }
-       }
-
-       public Integer[] window(int w, int h) {
-           return new Integer[]{600,800};
-       }
-
-       public Object[] button(Object uictx,
-                                 Object x, Object y, Object w, Object h,
-                                 String logic, String text, String image) {
-           Integer x2 = (Integer)x;
-           Log.d("filagree", "button " + x2 +","+ y +","+ w +","+ h +", logic="+ logic +", text="+ text +", image="+ image);
-           return new Object[]{null,22,33};
-       }
-   }
 
    /**
      *
@@ -93,7 +38,7 @@ public class Javagree {
 
        public int x = 6;
 
-       final public static String test_ui = " \n ui = sys.ui( nil, ['button', 'text':'clickez ici'] )";
+       final public static String test_ui = " \n ui = sys.ui( nil, ['label', 'text':'clickez ici'] )";
 
        public Integer y(Object a, Object b) {
            System.out.println("y: " + a +","+ b);
@@ -120,9 +65,9 @@ public class Javagree {
        }
    }
    
-   public static void doTesting(Context context) 
+   public static void doTesting(MainActivity activity) 
    {
-	   Sys sys = new Sys(context);
+	   Sys sys = new Sys(activity);
        Javagree a = new Javagree();
        Test test = new Test();
        a.eval(test, "tc", "sys.print('fg gets ' + tc.x + tc.z([7,8,9], ['p':99]))", null);
--- source/android/src/com/java/javagree/MainActivity.java
+++ source/android/src/com/java/javagree/MainActivity.java
@@ -1,26 +1,30 @@
 package com.java.javagree;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends Activity implements OnClickListener {
 
-	
+	public RelativeLayout layout;
+
+	/*	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.button1).setOnClickListener(this);
     }
+	 */
 
     @Override
     public void onClick(View v) {
-    	if(v.getId()==R.id.button1) {
+/*    	if(v.getId()==R.id.button1) {
      	       Javagree.doTesting(this);
-    	}
+    	} */
     }
 
     @Override
@@ -29,5 +33,11 @@ public class MainActivity extends Activity implements OnClickListener {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		this.layout = (RelativeLayout)findViewById(R.id.main_layout);
+	    Javagree.doTesting(this);
+	}
 }
