15c6269de714dfb5fd840485943d60dce59fc442
ys1382 <ys1382@gmail.com>
2014-02-15 11:11:47 -0800

    im login

--- platform/apple/OSXfgui/OSXfgui/AppDelegate.m
+++ platform/apple/OSXfgui/OSXfgui/AppDelegate.m
@@ -8,7 +8,8 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     //struct byte_array *filename = byte_array_from_string("../try.fg");
-    struct byte_array *filename = byte_array_from_string("sync_client.fg");
+//    struct byte_array *filename = byte_array_from_string("sync_client.fg");
+    struct byte_array *filename = byte_array_from_string("im_client.fg");
     struct byte_array *args     = byte_array_from_string("id='osx'");
     interpret_file(filename, args);
 }
